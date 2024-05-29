72f6b37c3bd167decfabaa4cf7f9b0742e00dbd1
Ramses Feliciano <ramsesfeliciano@gmail.com>
2024-05-29 14:34:10 -0400

    first commit

---
+++ Coffee shop registration lab/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+using Coffee_shop_registration_lab.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Coffee_shop_registration_lab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Form()
+        {
+            return View();
+        }
+        public IActionResult Registered(User u)
+        {
+                return View(u);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
